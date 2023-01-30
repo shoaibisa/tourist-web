be695b94351ff8dc8d8048514a4d9d46e75b7b20
Anubhav Rajput <72142278+Anubhav-Raj@users.noreply.github.com>
2023-01-31 00:20:46 +0530

    done

--- server.js
+++ server.js
@@ -6,10 +6,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-  res.render("index");
-});
-
+ app.get("/",(req,res)=>{
+   res.render("dashboard")
+ })
+ app.get("/guide/addpackage",(req,res)=>{
+  res.render("guide/addPackage")
+})
+app.get("/guide/packageList",(req,res)=>{
+  res.render("guide/packageList")
+})
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
