9082d64b9031cdba4467f565cedc23cf40bb351a
shoaibisa <shoaibisa1@gmail.com>
2023-02-20 10:51:25 +0530

    package list added

--- controller/package.js
+++ controller/package.js
@@ -1,12 +1,26 @@
 const Package = require("../model/package");
 
 exports.getAllPackages = (req, res, next) => {
-  Package.find()
+  Package.find({ status: "approved" })
     .populate("packageGuide")
     .exec()
     .then((packages) => {
       res.render("package/packages", {
         packages: packages,
+        profileImage: false,
+      });
+    });
+};
+
+exports.getPackage = (req, res, next) => {
+  const packageId = req.params.packageId;
+  Package.findById(packageId)
+
+    .populate("packageGuide")
+    .exec()
+    .then((pack) => {
+      res.render("package/singlepackage", {
+        pack: pack,
         profileImage: false,
       });
     });
--- routes/package.js
+++ routes/package.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const packageController = require("../controller/package");
 router.get("/packages", packageController.getAllPackages);
+router.get("/package/:packageId", packageController.getPackage);
 
 module.exports = router;
